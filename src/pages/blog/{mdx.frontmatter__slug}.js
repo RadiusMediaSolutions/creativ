7b00f2a815a87778721edd435b57e2b1025ecf58
BabuMar23 <babu@radiusmedia.com>
2023-02-13 09:35:05 +0530

    Minor change in frontmatter file

--- src/pages/blog/{mdx.frontmatter__slug}.js
+++ src/pages/blog/{mdx.frontmatter__slug}.js
@@ -67,6 +67,6 @@ export const query = graphql`
 	}
 `
 
-export const Head = ({ data }) => <Seo title={data.mdx.frontmatter.title} />
+// export const Head = ({ data }) => <Seo title={data.mdx.frontmatter.title} />
 
 export default BlogPost
