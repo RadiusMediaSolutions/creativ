4f12df88104c245a83f29049d795a7d9dff279dc
BabuMar23 <babu@radiusmedia.com>
2023-02-13 11:37:40 +0530

    commented seo stuffs

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,15 +1,16 @@
 import * as React from "react"
 
 import Layout from "../components/layout"
-import Seo from "../components/seo"
+
+// import Seo from "../components/seo"
 
 const NotFoundPage = () => (
-  <Layout>
-    <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
+	<Layout>
+		<h1>404: Not Found</h1>
+		<p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+	</Layout>
 )
 
-export const Head = () => <Seo title="404: Not Found" />
+// export const Head = () => <Seo title="404: Not Found" />
 
 export default NotFoundPage
